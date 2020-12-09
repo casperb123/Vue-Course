e0e1f18e665c2cde57a83af92e096645ddb6203e
Casper Bastholm <bastholm.casper@gmail.com>
2020-12-09 13:51:39 +0100

    Completed section 13

---
+++ routing-01-starting-setup/src/pages/UsersList.vue
@@ -0,0 +1,59 @@
+<template>
+  <button @click="confirmInput">Confirm</button>
+  <button @click="saveChanges">Save Changes</button>
+  <ul>
+    <user-item
+      v-for="user in users"
+      :key="user.id"
+      :name="user.fullName"
+      :role="user.role"
+    ></user-item>
+  </ul>
+</template>
+
+<script>
+import UserItem from '../components/users/UserItem';
+
+export default {
+  components: {
+    UserItem,
+  },
+  inject: ['users'],
+  data() {
+    return {
+      changesSaved: false,
+    };
+  },
+  methods: {
+    confirmInput() {
+      // do something
+      this.$router.push('/teams');
+    },
+    saveChanges() {
+      this.changesSaved = true;
+    },
+  },
+  beforeRouteEnter(to, from, next) {
+    console.log('UsersList Cpm beforeRouteEnter');
+    console.log(to, from);
+    next();
+  },
+  beforeRouteLeave(to, from, next) {
+    if (this.changesSaved) {
+      next();
+    } else {
+      const leave = confirm('Are you sure? You got unsaved changed!');
+      next(leave);
+    }
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  max-width: 20rem;
+  padding: 0;
+}
+</style>
